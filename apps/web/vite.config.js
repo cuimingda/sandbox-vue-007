75ab6db2f93dce5915993729d6be2936b841b3e1
cuimingda <cuimingda@gmail.com>
2024-10-17 00:13:00 +0800

    add Github Pages deploy action

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -4,13 +4,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const base = mode === 'production' ? '' : '/';
+  return {
+    base,
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
